f82d148694b5025580d015d7d7c55f5e01a1e477
Thor181 <art.torgaschin@yandex.ru>
2025-08-09 21:49:35 +0700

    [CSApp.V2a] added PortWorkerOptions and DbConnectionOptions

---
+++ CSApp.V2a/Services/Options/PortWorkerOptions.cs
@@ -0,0 +1,17 @@
+﻿using CSApp.V2a.Generated.Configuration;
+using CSLibrary.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSApp.V2a.Services.Options
+{
+
+    [Configuration(Section)]
+    public partial class PortWorkerOptions : IPortWorkerOptions
+    { 
+        public const string Section = "PortWorker";
+    }
+}
