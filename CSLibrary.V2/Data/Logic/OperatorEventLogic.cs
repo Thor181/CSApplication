2f9a7eb62c39480c92be199e4a3b5054bdcc981c
Thor181 <art.torgaschin@yandex.ru>
2025-08-10 20:48:50 +0700

    [CSLibrary.V2: OperatorEventLogic] added logic

---
+++ CSLibrary.V2/Data/Logic/OperatorEventLogic.cs
@@ -0,0 +1,30 @@
+﻿using CSLibrary.V2.Data.Models;
+using CSLibrary.V2.Stuff.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLibrary.V2.Data.Logic
+{
+    public class OperatorEventLogic : BaseLogic
+    {
+        public OperatorEventLogic(MfraDbContext dbContext) : base(dbContext) 
+        {
+            
+        }
+
+        public BaseResult WriteEvent(int typeId, int pointId)
+        {
+            var operatorEvent = new OperatorEvent()
+            {
+                PointId = pointId,
+                TypeId = typeId,
+            };
+
+            var result = base.Add(operatorEvent);
+            return result;
+        }
+    }
+}
