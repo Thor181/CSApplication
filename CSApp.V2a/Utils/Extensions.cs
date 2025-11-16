6adfff5269cc631e2e0d9172782e97132e3a7ece
Thor181 <art.torgaschin@yandex.ru>
2025-11-16 16:07:14 +0700

    [CSApp.V2a]
    Добавлен метод AddCustomJsonFile и рефакторинг
    
    Изменен способ загрузки конфигурации: добавлен метод
    AddCustomJsonFile для поддержки пользовательского пути к
    файлу настроек. В проекте удалена папка `Models` и ресурс
    `Assets\Sounds\error.wav`. В `_appSettings.json` добавлено
    поле `"Path"` для указания пользовательского пути.
    Добавлен класс `CustomAppSettingsPath` для десериализации
    пути к пользовательскому файлу настроек.

--- CSApp.V2a/Utils/Extensions.cs
+++ CSApp.V2a/Utils/Extensions.cs
@@ -1,14 +1,41 @@
 ﻿using Avalonia.Rendering.Composition.Animations;
+using CSApp.V2a.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CSApp.V2a.Utils
 {
     public static class Extensions
     {
+        public static IConfigurationBuilder AddCustomJsonFile(this IConfigurationBuilder builder)
+        {
+            const string appSettingsPath = "_appSettings.json";
+            if (File.Exists(appSettingsPath))
+            {
+                var json = File.ReadAllText(appSettingsPath);
+                var path = JsonSerializer.Deserialize<CustomAppSettingsPath>(json)?.Path;
+
+                if (!string.IsNullOrEmpty(path))
+                {
+                    if (!File.Exists(path))
+                        throw new FileNotFoundException($"The custom app settings file was not found at the specified path: {path}");
+
+                    builder.AddJsonFile(path);
+                }
+                else
+                {
+                    builder.AddJsonFile(appSettingsPath);
+                }
+            }
+
+            return builder;
+        }
     }
 }
